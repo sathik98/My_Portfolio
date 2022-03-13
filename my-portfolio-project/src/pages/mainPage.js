bacf01042c7dde38324f8889bd346a874911ea03
sathik <sathiksatk98@gmail.com>
2022-03-13 09:50:11 +0530

    heading update

---
+++ my-portfolio-project/src/pages/mainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MainPage() {
+    return (
+    <>
+    <div className='heading_one'>
+        <p>my portfolio</p>
+    </div>
+    </>
+  )
+}
+
+export default MainPage;
